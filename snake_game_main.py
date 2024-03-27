a4e6b04c66efb9d18feb0e7e6d465736ea009e8d
PIETRO-ing <pa.pietro81@gmail.com>
2024-03-27 16:49:23 +0100

    updt main with methods from Snake class: create and move

--- snake_game_main.py
+++ snake_game_main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake_game_snake import Snake
 import time
 
 screen = Screen()
@@ -7,27 +8,14 @@ screen.bgcolor('black')
 screen.title('My Snake Game')
 screen.tracer(0)
 
-segments = []
-#turtle dimension is 20px X 20px
-for i in range(0,-60,-20):
-   new_segment =Turtle(shape='square')
-   new_segment.color('white')
-   new_segment.penup()
-   new_segment.goto(i,0)
-   # new_segment.speed(1)
-   segments.append(new_segment)
+snake = Snake()
+snake.create_snake()
 
 game_is_on = True
 while game_is_on:
    screen.update()
    time.sleep(0.1)
-   
-   for i in range(2,0,-1):
-      new_x_cor = segments[i - 1].xcor()
-      new_y_cor = segments[i - 1].ycor()
-      segments[i].goto(new_x_cor,new_y_cor)
-   segments[0].forward(20)
-   segments[0].left(90)
+   snake.move()
    
 
 screen.exitonclick()
